ef6dba9ef29e86aac59fd8e48e577d0997bacc30
Socketlike <36399055+Socketlike@users.noreply.github.com>
2023-07-24 10:45:03 +0700

    chore(/replugged): use hyperlink to refer to JSON schema

--- src/pages/replugged/Body.tsx
+++ src/pages/replugged/Body.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { TooltippedText } from '@components'
 
 export const Body = (): JSX.Element => (
   <div className='replugged-body'>
     <span>
       Welcome to my domain of Replugged shenanigans!
       <br />
+      <br />
       There is nothing much here except for a{' '}
-      <TooltippedText tooltip='It is at /addon.schema.json'>JSON schema</TooltippedText> at the
-      moment.
+      <a href='./addon.schema.json' className='block'>
+        JSON schema
+      </a>{' '}
+      at the moment.
     </span>
   </div>
 )
